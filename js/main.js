7995eda9c88e6b4780f9bb347437109c18ae1066
harshal-singh <dharma3136@gmail.com>
2021-09-24 22:59:29 +0530

    add share option

--- js/main.js
+++ js/main.js
@@ -1,29 +1,38 @@
 const button = document.getElementById("share");
+const error = document.getElementById("error");
 
 // download image
-function share(url, imageName) {
+function share(url) {
     fetch(url)
         .then((data) => {
             return data.blob();
         })
         .then((imgBlob) => {
-            const myImage = document.createElement("img");
-            const objectURL = URL.createObjectURL(imgBlob);
-            myImage.src = objectURL;
+            var file = new File([imgBlob], "share-image.jpg", { type: "image/jpeg" });
+            var filesArray = [file];
+            var shareData = { files: filesArray };
 
-            // const a = document.createElement("a");
-            // a.setAttribute("download", imageName);
-            // a.setAttribute("href", objectURL);
+            if (navigator.canShare && navigator.canShare(shareData)) {
+                // Adding title afterwards as navigator.canShare just
+                // takes files as input
+                // shareData.title = "Name";
 
-            const a = `<a href="whatsapp://send?text=${encodeURIComponent(objectURL)}">Share on whatsapp!</a>`;
-            document.querySelector("body").insertAdjacentHTML("beforeend", a);
+                navigator
+                    .share(shareData)
+                    .then(() => (error.textContent = "Share was successful."))
+                    .catch((err) => (error.textContent = "Sharing failed: " + err));
+            } else {
+                error.textContent = "Your system doesn't support sharing files.";
+            }
         })
         .catch((err) => {
-            console.log(err);
+            error.textContent = err;
         });
 }
 
 button.onclick = (e) => {
     e.preventDefault();
-    share("https://images.dog.ceo/breeds/weimaraner/n02092339_3979.jpg", "dog");
+    button.style.background = "green";
+    share("https://images.dog.ceo/breeds/weimaraner/n02092339_3979.jpg");
+    error.textContent = "click";
 };
