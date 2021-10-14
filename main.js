53251a63fcfc1d7c37cd2508ed74250a1a4a545a
harshal-singh <dharma3136@gmail.com>
2021-10-14 11:45:31 +0530

    add share option

--- main.js
+++ main.js
@@ -116,45 +116,24 @@ function shareImageText(url) {
             return data.blob();
         })
         .then((imageBlob) => {
-            fetch("/sound.wav")
-                .then((data) => {
-                    return data.blob();
-                })
-                .then((soundBlod) => {
-                    let shareImageText = new MozActivity({
-                        //Name of activity that set the ringtone
-                        name: "share",
-                        data: {
-                            //Type of file, in that case acept all audio formats
-                            // type: "audio/*",
-                            number: 1,
-                            //The blob object of audio file, must be within in array
-                            blobs: [soundBlod],
-                            //The metadata of audio, these are the 3 available properties
-                            metadata: [
-                                {
-                                    title: "Umbrella",
-                                    artist: "Rihanna",
-                                    //The picture must be blob object
-                                    picture: imageBlob,
-                                },
-                            ],
-                        },
-                    });
+            let shareImageText = new MozActivity({
+                //Name of activity that set the ringtone
+                name: "share",
+                data: {
+                    type: "text/html",
+                    html_text: `<img src="${URL.createObjectURL(imageBlob)}" alt="image">`,
+                },
+            });
 
-                    //Success handler
-                    shareImageText.onsuccess = function () {
-                        error.textContent = "Success share image and text!";
-                    };
+            //Success handler
+            shareImageText.onsuccess = function () {
+                error.textContent = "Success share image and text!";
+            };
 
-                    //Error handler
-                    shareImageText.onerror = function () {
-                        error.textContent = this.error;
-                    };
-                })
-                .catch((err) => {
-                    error.textContent = "Sound: " + err;
-                });
+            //Error handler
+            shareImageText.onerror = function () {
+                error.textContent = this.error;
+            };
         })
         .catch((err) => {
             error.textContent = "Image: " + err;
