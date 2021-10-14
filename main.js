ba7d29e6d35a0aab354ca133bb63832de5550b6e
harshal-singh <dharma3136@gmail.com>
2021-10-14 17:20:35 +0530

    add share option

--- main.js
+++ main.js
@@ -4,31 +4,30 @@ const btnShare = document.getElementById("share");
 const btnShareText = document.getElementById("share-text");
 const btnShareImage = document.getElementById("share-image");
 const btnShareImageText = document.getElementById("share-image-text");
-
-const textBlob = new Blob(["https://harshal-singh.github.io/share"], { type: "text/plain" });
+const image1 = "https://image.freepik.com/free-photo/blue-concrete-wall-textures-background_74190-7757.jpg";
+const image2 = "https://image.freepik.com/free-vector/flat-design-red-comic-style-background_23-2148797742.jpg";
 
 btnShare.onclick = (e) => {
     e.preventDefault();
     btnShare.style.background = "red";
-
-    share("https://image.freepik.com/free-vector/flat-design-red-comic-style-background_23-2148797742.jpg");
+    share();
 };
 
 btnShareText.onclick = (e) => {
     e.preventDefault();
     btnShareText.style.background = "blue";
-    shareText();
+    shareText(image1);
 };
 
 btnShareImage.onclick = (e) => {
     e.preventDefault();
     btnShareImage.style.background = "green";
-    shareImage("https://image.freepik.com/free-photo/blue-concrete-wall-textures-background_74190-7757.jpg");
+    shareImage(image1, image2);
 };
 
 btnShareImageText.onclick = (e) => {
     e.preventDefault();
-    btnShareImageText.style.background = "brown";
+    btnShareImageText.style.background = "yellow";
     shareImageText("https://img.freepik.com/free-vector/abstract-yellow-comic-zoom_1409-923.jpg?size=626&ext=jpg");
 };
 
@@ -40,7 +39,8 @@ function share(url) {
         })
         .then((imgBlob) => {
             const image = new File([imgBlob], "share-image.jpg", { type: "image/jpeg" });
-            const text = new File([textBlob], "share-text.txt", { type: "text/plain" });
+            const textBlob = new Blob(["https://harshal-singh.github.io/stop-watch"], { type: "text/plain" });
+            const text = new File([textBlob], "share-image.jpg", { type: "image/jpeg" });
             const filesArray = [image, text];
             const shareData = { files: filesArray };
 
@@ -59,44 +59,48 @@ function share(url) {
 }
 
 // mozilla share image
-function shareImage(url) {
-    fetch(url)
+function shareImage(url1, url2) {
+    const image1Blob = fetch(url1)
         .then((data) => {
             return data.blob();
-        })
-        .then((imageBlob) => {
-            const image = new File([imageBlob], "share-image.jpg", { type: "image/jpeg" });
-            const text = new File([textBlob], "", { type: "text/plain" });
-            const filesArray = [image, text];
-            const shareData = { files: filesArray };
-
-            // share image
-            var shareImage = new MozActivity({
-                name: "share",
-                data: {
-                    // type: ["image/*", "text/plain"],
-                    // number: 1,
-                    blobs: [new Blob(shareData)],
-                },
-            });
-
-            // image share successfully
-            shareImage.onsuccess = function () {
-                error.textContent = "Success share image!";
-            };
-
-            // error in sharing image
-            shareImage.onerror = function () {
-                error.textContent = this.error;
-            };
         })
         .catch((err) => {
             error.textContent = err;
         });
+
+    const image2Blob = fetch(url2)
+        .then((data) => {
+            return data.blob();
+        })
+        .catch((err) => {
+            error.textContent = err;
+        });
+
+    // share image
+    var shareImage = new MozActivity({
+        name: "share",
+        data: {
+            type: ["image/*"],
+            number: 1,
+            blobs: [image1Blob, image2Blob],
+        },
+    });
+
+    // image share successfully
+    shareImage.onsuccess = function () {
+        error.textContent = "Success share image!";
+        body.style.backgroundImage = `linear-gradient(to top left, yellow, yellow)`;
+    };
+
+    // error in sharing image
+    shareImage.onerror = function () {
+        error.textContent = this.error;
+    };
 }
 
 function shareText() {
     // share text
+    const textBlob = new Blob(["https://harshal-singh.github.io/stop-watch"], { type: "text/plain" });
     var shareText = new MozActivity({
         name: "share",
         data: {
@@ -108,43 +112,11 @@ function shareText() {
     // text share successfully
     shareText.onsuccess = function () {
         error.textContent = "Success share text!";
+        body.style.backgroundImage = `linear-gradient(to top left, orangered, orangered)`;
     };
 
     // error in sharing text
-    shareText.onerror = function () {
+    shareText.onerror = function (err) {
         error.textContent = this.error;
     };
-}
-
-function shareImageText(url) {
-    fetch(url)
-        .then((data) => {
-            return data.blob();
-        })
-        .then((imageBlob) => {
-            const image = new File([imageBlob], "share-image.jpg", { type: "image/jpeg" });
-            const text = new File([textBlob], "", { type: "text/plain" });
-            const filesArray = [image, text];
-            const shareData = { files: filesArray };
-
-            let shareImageText = new MozActivity({
-                name: "share",
-                data: {
-                    blobs: filesArray,
-                },
-            });
-
-            //Success handler
-            shareImageText.onsuccess = function () {
-                error.textContent = "Success share image and text!";
-            };
-
-            //Error handler
-            shareImageText.onerror = function () {
-                error.textContent = this.error;
-            };
-        })
-        .catch((err) => {
-            error.textContent = "Image: " + err;
-        });
 }
